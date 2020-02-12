2fa869854bb3c59a021907d5fb28e3f92482465b
wais <waisalmakaleh@gmail.com>
2020-02-12 17:06:13 -0500

    fixed

--- index.js
+++ index.js
@@ -67,14 +67,31 @@ server.delete( '/api/hubs/:id', ( req, res ) =>{
 server.put('/api/hubs/:id', (req, res) => {
  const id = req.params.id
  const body = req.body
- hubs.update(id, body)
+ const {name, bio} = req.body;
+
+  if (!name || !bio) {
+    res.status(404).json({ errorMessage: "Please provide name and bio for the user." });
+    return;
+  }
+
+ hubs.update(id, {name, bio})
  .then( updated =>{
-   res.status(200).json(updated)
+   if (updated) {
+     hubs.findById(id)
+      .then(user => res.status(200).json(user))
+      .catch(err => {
+        console.log(err)
+        res.status(500).json({error: "cannot be removed"})
+        return;
+      })
+   } else {
+     res.status(404).json({message: 'user with that id does not exist'});
+     return;
+   }
   })
   .catch( err =>{
    res.status( 500 ).json( err );
   });
-
 })
 
 
